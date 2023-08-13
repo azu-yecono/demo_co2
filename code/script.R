f472a1f8253df1de3a0be1930335c68cf11a4c3b
t-yoshimura0123@g.ecc.u-tokyo.ac.jp <t-yoshimura0123@g.ecc.u-tokyo.ac.jp>
2023-08-13 11:29:51 +0900

    lines of 7 countries

--- code/script.R
+++ code/script.R
@@ -8,11 +8,11 @@ data$gdp_per_capita <- data$gdp / data$population
 data |> 
   dplyr::filter(
     year < 2016 & year > 1819 &
-    country == c("United States", "Japan"))|>
+    country == c("United States", "United Kingdom","Germany","France","Japan","Canada","Italy"))|>
   ggplot2::ggplot(
     mapping=aes(x=gdp_per_capita,
                 y=co2_per_capita,
                 color = factor(country)),
     group_by(country))+
-  geom_point()
+  geom_line()
 plot
